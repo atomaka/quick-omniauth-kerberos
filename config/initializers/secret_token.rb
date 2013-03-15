06b0698fb35cca45c067188973abe481754cbddb
Andrew Tomaka <atomaka@gmail.com>
2013-03-15 08:58:42 -0400

    "rails n kerb"; commit of plain rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kerb::Application.config.secret_token = '73c234fbe4abf2cf7e423ca324fc533a4a3315ab776d47c2e76fa610eec8a90a5285c24a1fb2878c286e016ba9a12a162b61411b583d8e93642017a2660c6d75'
